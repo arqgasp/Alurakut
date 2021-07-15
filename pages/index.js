b81e1f8585e9c88a759cf6a74e47e3e17a819c72
arqgasp <gabis@MacBook-Pro-de-Gabriel.local>
2021-07-14 21:55:17 -0300

    aula02

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar (propriedades) {
@@ -9,12 +10,29 @@ function ProfileSidebar (propriedades) {
   return (
     <Box>
     <img src={`https://github.com/${propriedades.githubUser}.png`} style= {{borderRadius: '8px'}}/>
+    <hr />
+
+    <p> 
+    <a className="boxLink" href={`https://github.com/${propriedades.githubUser}`}>
+@{propriedades.githubUser}
+    </a>
+    </p>
+    <hr />
+
+    <AlurakutProfileSidebarMenuDefault />
      </Box> 
   )
 }
 
 export default function Home() {
 const usuarioAleatorio = 'arqgasp' ;
+const [comunidades, setComunidades] = React.useState([{
+  id: '85948965896586588',
+  title: 'Eu odeio acordar cedo',
+  image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+}]);
+console.log('Nosso teste', );
+// const comunidades = ['Alurakut'];
 const pessoasFavoritas = ['marcobrunodev', 
 'omariosouto', 
 'peas', 
@@ -37,8 +55,62 @@ const pessoasFavoritas = ['marcobrunodev',
     </h1>
     <OrkutNostalgicIconSet />
    </Box>
+
+<Box>
+<h2 className="subTitle"> O que você deseja fazer?</h2>
+<form onSubmit ={function handleCriaComunidade(e) {
+e.preventDefault();
+const dadosDoForm = new FormData(e.target);
+
+console.log('Campo: ', dadosDoForm.get('title'));
+console.log('Campo: ', dadosDoForm.get('image'));
+
+const comunidade = {
+  id: new Date().toISOString(), 
+title: dadosDoForm.get('title'),
+image: dadosDoForm.get('image'),
+}
+const comunidadesAtualizadas = [...comunidades, comunidade];
+setComunidades(comunidadesAtualizadas);
+}}>
+  <div>
+  <input 
+  placeholder="Qual vai ser o nome da sua comunidade?" name="title" 
+  aria-label="Qual vai ser o nome da sua comunidade?"
+  type="text"
+  />
+  </div>
+  <div>
+  <input 
+  placeholder="Coloque uma URL para usarmos de capa?" name="image" 
+  aria-label="Qual vai ser o nome da sua comunidade?"
+  />
+  </div>
+
+  <button>
+    Criar comunidade
+  </button>
+</form>
+   </Box>
   </div>
   <div className="profileRelationsArea" style={{gridArea: `profileRelationsArea`}}>
+    <ProfileRelationsBoxWrapper>
+    <h2 className="smallTitle">
+      Comunidades ({comunidades.length})
+      </h2>
+    <ul>
+     {comunidades.map((itemAtual) => {
+       return (
+         <li key={itemAtual.id}>
+          <a href={`/users/${itemAtual.title}`} key={itemAtual.title}>
+            <img src={itemAtual.image} />
+            <span>{itemAtual.title}</span>
+          </a>
+         </li>
+       )
+     })}
+     </ul>
+    </ProfileRelationsBoxWrapper>
     <ProfileRelationsBoxWrapper>
       <h2 className="smallTitle">
       Pessoas da comunidade ({pessoasFavoritas.length})
@@ -47,8 +119,8 @@ const pessoasFavoritas = ['marcobrunodev',
     <ul>
      {pessoasFavoritas.map(( itemAtual) => {
        return (
-         <li>
-          <a href={`/users/${itemAtual}`} key={itemAtual}>
+         <li key={itemAtual}>
+          <a href={`/users/${itemAtual}`} >
             <img src={`https://github.com/${itemAtual}.png`} />
             <span>{itemAtual}</span>
           </a>
